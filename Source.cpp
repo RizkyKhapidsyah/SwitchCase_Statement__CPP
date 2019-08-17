f5e7340fa4ec68ba6ee2baa628992c63c60b35fa
Rizky Hafitsyah <45317201+RizkyKhapidsyah@users.noreply.github.com>
2019-08-18 00:56:50 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "Masukkan Nilai A = "; cin >> a;
+
+	switch (a)
+	{
+	case 1:
+		cout << "A = 1" << endl;
+		break;
+	case 2:
+		cout << "A = 2" << endl;
+		break;
+	case 3:
+		cout << "A = 3" << endl;
+		break;
+	default:
+		cout << "Nilai A tidak ada di daftar, namun nilai yang terdeteksi adalah : " << a << endl;
+		break;
+	}
+
+	_getch();
+	return 0;
+}
